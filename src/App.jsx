9a83a63367a843d9bbfe72ddc2b7dd7f16cd0854
Savkiv <ivan.savkiv.igorovych>
2022-04-18 10:25:08 +0300

    Add useReduserHook

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import { Car } from "./components/useStateHook";
 import { Timer } from "./components/useEffectHook";
+import { Todo } from "./components/useReducerHook";
 
 function App() {
   return (
     <>
       <Car />
       <Timer />
+      <Todo />
     </>
   );
 }
